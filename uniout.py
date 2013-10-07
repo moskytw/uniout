52f469999d68fefa0806a1071cd5f48356c85dd2
Mosky <mosky.tw@gmail.com>
2013-10-07 12:56:24 +0800

    sys should be preceding

--- uniout.py
+++ uniout.py
@@ -3,8 +3,8 @@
 
 __version__ = '0.3'
 
-from _uniout import Uniout, runs_in_ipython
 import sys
+from _uniout import Uniout, runs_in_ipython
 
 if runs_in_ipython():
     from IPython.utils import io
