bec505ba26d9e3f1f8b2a67100c9bda97bd0b2a9
Mosky <mosky.tw@gmail.com>
2013-10-11 04:11:46 +0800

    added docs

--- _uniout.py
+++ _uniout.py
@@ -5,6 +5,17 @@ import sys
 import re
 
 def literalize(content, is_unicode=False):
+    '''Literalize the content.
+
+    Examples:
+
+    >>> print literalize('str')
+    'str'
+    >>> print literalize('\'str\'')
+    "'str'"
+    >>> print literalize('\"\'str\'\"')
+    '"\'str\'"'
+    '''
 
     quote_mark = "'"
     if "'" in content:
@@ -19,6 +30,16 @@ string_literal_re = re.compile(r'''(?<![uU])(?P<q>['"]).+?(?<!\\)(?P=q)''')
 unicode_literal_re = re.compile(r'''[uU](?P<q>['"]).+?(?<!\\)(?P=q)''')
 
 def unescape_string_literal(b, target_encoding):
+    '''Unescape string literal.
+
+    Examples:
+
+    >>> print unescape_string_literal(r"'\xe4\xb8\x96\xe7\x95\x8c\xe4\xbd\xa0\xe5\xa5\xbd'", 'utf-8')
+    '世界你好'
+
+    >>> print unescape_string_literal(r"'\xa5@\xac\xc9\xa7a\xa6n'", 'utf-8')
+    '\xa5@\xac\xc9\xa7a\xa6n'
+    '''
 
     b = b[1:-1].decode('string-escape')
 
@@ -30,23 +51,32 @@ def unescape_string_literal(b, target_encoding):
     return literalize(b)
 
 def unescape_unicode_literal(b, target_encoding):
+    '''Unescape unicode literal.
+
+    Examples:
+
+    >>> print unescape_unicode_literal(r"u'\u4e16\u754c\u4f60\u597d'", 'utf-8')
+    u'世界你好'
+    '''
+
     return literalize(
         b[2:-1].decode('unicode-escape').encode(target_encoding),
         is_unicode=True
     )
 
 def unescape(b, target_encoding=None):
+    '''Unescape all string and unicode literals.'''
 
     if target_encoding is None:
         target_encoding = sys.stdout.encoding
 
     b = string_literal_re.sub(lambda m: unescape_string_literal(m.group(), target_encoding), b)
-
     b = unicode_literal_re.sub(lambda m: unescape_unicode_literal(m.group(), target_encoding), b)
 
     return b
 
 def make_unistream(stream):
+    '''Make a stream which unescapes literals before to write.'''
 
     unistream = lambda: 'middleware'
 
