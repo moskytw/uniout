d8ed3f60820b67abfef1c5fc0ad155c1a101bec6
Mosky <mosky.tw@gmail.com>
2013-10-11 11:38:04 +0800

    merged the two unesacpe functions

--- _uniout.py
+++ _uniout.py
@@ -25,50 +25,45 @@ def literalize(content, is_unicode=False):
 
     return 'u'[not is_unicode:]+quote_mark+content+quote_mark
 
-string_literal_re = re.compile(r'''(?<![uU])(?P<q>['"]).+?(?<!\\)(?P=q)''')
-unicode_literal_re = re.compile(r'''[uU](?P<q>['"]).+?(?<!\\)(?P=q)''')
+string_literal_re = re.compile(r'''[uU]?(?P<q>['"]).+?(?<!\\)(?P=q)''')
 
 def unescape_string_literal(b, target_encoding):
-    '''Unescape string literal.
+    '''Unescape string or unicode literal.
 
     Examples:
 
-    >>> print unescape_string_literal(r"'\xe4\xb8\x96\xe7\x95\x8c\xe4\xbd\xa0\xe5\xa5\xbd'", 'utf-8')
+    >>> u = u'世界你好'
+    >>> print unescape_string_literal(repr(u), 'utf-8')
+    u'世界你好'
+
+    >>> print unescape_string_literal(repr(u.encode('utf-8')), 'utf-8')
     '世界你好'
 
-    >>> print unescape_string_literal(r"'\xa5@\xac\xc9\xa7a\xa6n'", 'utf-8')
+    >>> print unescape_string_literal(repr(u.encode('big5')), 'utf-8')
     '\xa5@\xac\xc9\xa7a\xa6n'
     '''
 
-    b = b[1:-1].decode('string-escape')
+    if b[0] in 'uU':
 
-    try:
-        b.decode(target_encoding)
-    except UnicodeDecodeError:
-        b = b.encode('string-escape')
+        return literalize(
+            b[2:-1].decode('unicode-escape').encode(target_encoding),
+            is_unicode=True
+        )
+
+    else:
+
+        b = b[1:-1].decode('string-escape')
+
+        try:
+            b.decode(target_encoding)
+        except UnicodeDecodeError:
+            b = b.encode('string-escape')
 
     return literalize(b)
 
-def unescape_unicode_literal(b, target_encoding):
-    '''Unescape unicode literal.
-
-    Examples:
-
-    >>> print unescape_unicode_literal(r"u'\u4e16\u754c\u4f60\u597d'", 'utf-8')
-    u'世界你好'
-    '''
-
-    return literalize(
-        b[2:-1].decode('unicode-escape').encode(target_encoding),
-        is_unicode=True
-    )
-
 def unescape(b, target_encoding):
     '''Unescape all string and unicode literals.'''
-
-    b = string_literal_re.sub(lambda m: unescape_string_literal(m.group(), target_encoding), b)
-    b = unicode_literal_re.sub(lambda m: unescape_unicode_literal(m.group(), target_encoding), b)
-    return b
+    return string_literal_re.sub(lambda m: unescape_string_literal(m.group(), target_encoding), b)
 
 def make_unistream(stream):
     '''Make a stream which unescapes literals before to write.'''
